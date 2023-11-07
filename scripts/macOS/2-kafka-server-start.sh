55d0f8b3a37a7f06bae3e0263b2ea6afcbf57dda
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2023-11-06 21:21:41 -0300

    Changed the file name that points to a .properties file.

--- scripts/macOS/2-kafka-server-start.sh
+++ scripts/macOS/2-kafka-server-start.sh
@@ -1,1 +1,1 @@
-sudo /Users/yannap./Documents/confluent-7.5.1/bin/kafka-server-start /Users/yannap./Documents/confluent-7.5.1/etc/kafka/server3.properties
+sudo /Users/yannap./Documents/confluent-7.5.1/bin/kafka-server-start /Users/yannap./Documents/confluent-7.5.1/etc/kafka/server2.properties
